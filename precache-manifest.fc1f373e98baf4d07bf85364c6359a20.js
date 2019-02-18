8af2669c7b89b140f4eb2838c6b0de713c1f6107
SystemGhost <jackie07364538@gmail.com>
2019-02-18 20:56:53 +0300

    deploy project

---
+++ precache-manifest.fc1f373e98baf4d07bf85364c6359a20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "f9db4a2cbd2d7f18665f",
+    "url": "/react-app/static/js/main.f9db4a2c.chunk.js"
+  },
+  {
+    "revision": "4907012ebcc70f57c31a",
+    "url": "/react-app/static/js/1.4907012e.chunk.js"
+  },
+  {
+    "revision": "f9db4a2cbd2d7f18665f",
+    "url": "/react-app/static/css/main.203655e5.chunk.css"
+  },
+  {
+    "revision": "7a866f41687ff8fabd4c8e35a38e676f",
+    "url": "/react-app/index.html"
+  }
+];
